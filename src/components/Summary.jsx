3be83aa02a27badafc7ad676e279401fb0811332
ryansaballa <154952941+ryansaballa@users.noreply.github.com>
2026-08-30 01:26:14 +0000

    updated summary - include items in object

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -3,22 +3,43 @@ export default function Summary({ checklist }) {
     {
       key: "affordInvest",
       label: "Invest What's Left",
+      description: "to find more about the basics of investing, check ",
+      title: "Investing Basics",
+      link: "https://www.ciro.ca/office-investor/investing-basics",
+      image: "",
     },
     {
       key: "diversifyInvest",
       label: "Diversifying Your Investments",
+      description: "to find more about the basics of investing, check ",
+      title: "Diversifying Your Investments",
+      link: "https://www.ciro.ca/office-investor/investing-basics/why-diversify-your-portfolio",
+      image: "",
+      
     },
     {
       key: "investLong",
       label: "Investing For the Long-term",
+      description: "to find more about the basics of investing, check ",
+      title: "Compound Interest",
+      link: "https://www.ciro.ca/office-investor/investing-basics/compound-interest",
+      image: "",
     },
     {
       key: "understandRisk",
       label: "Understanding Risk and Returns",
+      description: "to find more about the basics of investing, check ",
+      title: "Understanding Risk",
+      link: "https://www.ciro.ca/office-investor/understanding-risk",
+      image: "",
     },
     {
       key: "understandInvest",
       label: "Understanding Investments",
+      description: "explore opportunities for investing by",
+      title: "Understanding Investment Performance and Returns",
+      link: "https://www.ciro.ca/office-investor/investing-basics/understanding-investment-performance-returns",
+      image: "",
     },
   ];
   const selectedItems = checklistItems.filter((item) => checklist[item.key]);
@@ -31,7 +52,7 @@ export default function Summary({ checklist }) {
       {selectedItems.length > 0 ? (
         <ul>
           {selectedItems.map((item) => (
-            <li key={item.key}>{item.label}</li>
+            <li key={item.key}>{item.label} - {item.description}<a href="${item.link}">{item.title}</a></li>
           ))}
         </ul>
       ) : (
