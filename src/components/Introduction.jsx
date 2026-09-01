6aa116cdfe8fbd0a24bcfc43ef9e6658cd17ec25
Ryan S <ryanperry.saballa@gmail.com>
2026-08-31 22:47:37 -0400

    update css - intro

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -3,28 +3,47 @@ export default function Introduction() {
     <>
       <section className="introduction">
         <h2>Canadian Investment Regulatory Organization</h2>
-        <div>
-          <p>
-            We are the Canadian Investment Regulatory Organization, or CIRO
-            (“see - roe”) for short.
-          </p>
-          <p>
-            We oversee investment dealers, mutual fund dealers and trading
-            activity on Canada’s debt and equity marketplaces. CIRO is committed
-            to the protection of investors, providing efficient and consistent
-            regulation, and building Canadians’ trust in financial regulation
-            and the people managing their investments
-          </p>
-        </div>
-        <div>
-          <p>
-            Now that you’re here, we want to help you start investing towards
-            achieving your Canadian dream.
-          </p>
-          <p>
-            We’ve prepared this guide to give you an overview of your investment
-            options, who can help, and how you can protect yourself from scams
-          </p>
+        <div className="main-intro">
+          <div className="about-ciro">
+            <div>
+              <p>
+                We are committed to the protection of investors, providing
+                efficient and consistent regulation, and building Canadians’
+                trust in financial regulation and the people managing their
+                investments.
+              </p>
+            </div>
+            <div>
+              <p>
+                This guide will walk through your investment options and how you
+                can protect yourself from scams.
+              </p>
+            </div>
+          </div>
+          <div className="about-ooti">
+            <div>
+              <h3>Office of the Investor Mandate</h3>
+              <ul>
+                <li>Support policy development</li>
+                <li>Educate investors</li>
+                <li>Engage in investor outreach</li>
+                <li>Perform investor research</li>
+                <li>Support the Investor Advisory Panel (IAP)</li>
+              </ul>
+            </div>
+            <div>
+              <h3>Contact</h3>
+              <p>
+                To inquire about our outreach sessions or connect with us on
+                investor education or outreach initiatives, please contact us
+                at:&nbsp;
+                <a href="mailto:officeoftheinvestor@ciro.ca" target="_self">
+                  officeoftheinvestor@ciro.ca
+                </a>
+                .
+              </p>
+            </div>
+          </div>
         </div>
       </section>
     </>
