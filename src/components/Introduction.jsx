4cf4ce83c653dced5a87304fc7bf448f4316cdab
ryansaballa <154952941+ryansaballa@users.noreply.github.com>
2026-09-03 19:58:25 +0000

    update welcome layout css

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -4,43 +4,39 @@ export default function Introduction() {
       <section className="introduction">
         <div className="main-intro">
           <div className="about-ooti">
-            <div>
-              <h2>Office of the Investor</h2>
-              <p>
-                We are committed to the protection of investors and building
-                Canadians’ trust in managing their investments.
-              </p>
-              <div>
-                <h2>Connect With Us</h2>
-                <p>
-                  For investor education or outreach initiatives, connect with
-                  us at:&nbsp;
-                  <a href="mailto:officeoftheinvestor@ciro.ca" target="_self">
-                    officeoftheinvestor@ciro.ca
-                  </a>
-                  .
-                </p>
-              </div>
-            </div>
+            <h2>Office of the Investor</h2>
+            <p>
+              We are committed to the protection of investors and building
+              Canadians’ trust in managing their investments.
+            </p>
+            <p>
+              <strong>Connect With Us</strong>
+            </p>
+            <p>
+              For investor education or outreach initiatives, connect with us
+              at:&nbsp;
+              <a href="mailto:officeoftheinvestor@ciro.ca" target="_self">
+                officeoftheinvestor@ciro.ca
+              </a>
+              .
+            </p>
           </div>
-          <div className="ooti-connect">
-            <div>
-              <p>Our Mandate:</p>
-              <ul>
-                <li>Support policy development</li>
-                <li>Educate investors</li>
-                <li>Engage in investor outreach</li>
-                <li>Perform investor research</li>
-                <li>Support the Investor Advisory Panel (IAP)</li>
-              </ul>
-              <p>
-                This guide will walk through your investment options and how you
-                can protect yourself from scams.
-              </p>
-            </div>
+          <div className="ooti-guide">
+            <p>Our Mandate:</p>
+            <ul>
+              <li>Support policy development</li>
+              <li>Educate investors</li>
+              <li>Engage in investor outreach</li>
+              <li>Perform investor research</li>
+              <li>Support the Investor Advisory Panel (IAP)</li>
+            </ul>
+            <p>
+              This guide will walk through your investment options and how you
+              can protect yourself from scams.
+            </p>
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
