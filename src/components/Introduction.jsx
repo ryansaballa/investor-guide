1698a6b355aac3c71caa2831c22023220e1b1f94
Ryan S <ryanperry.saballa@gmail.com>
2026-09-04 18:00:26 -0400

    update layout - ul

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -30,13 +30,15 @@ export default function Introduction() {
               <li>Perform investor research</li>
               <li>Support the Investor Advisory Panel (IAP)</li>
             </ul>
-            <p>
-              This guide will walk through your investment options and how you
-              can protect yourself from scams.
-            </p>
+            <div>
+              <p>
+                This guide will walk through your investment options and how you
+                can protect yourself from scams.
+              </p>
+            </div>
           </div>
         </div>
       </section>
     </>
-  );
+  )
 }
